75e99099d609884cafbaef8f420239346a84eb46
Ayush Gupta <ayush2425.rk@gmail.com>
2025-02-03 02:18:05 +0530

    Add HTML and JavaScript files for conditional statements example

---
+++ JavaScript/conditions.js
@@ -0,0 +1,14 @@
+console.log("Before my if statement");
+let age=23;
+if(age>=18){
+    console.log("You are an adult");
+    console.log("You can drive");
+}
+else{
+    console.log("You are a minor");
+    console.log("You cannot drive");
+}
+if(age>20){
+    console.log("You are in 20s");
+}
+console.log("After my if statement");
