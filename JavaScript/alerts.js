0894bb84a2c0e743c9770579939b2817ffde1fd7
Ayush Gupta <ayush2425.rk@gmail.com>
2025-02-03 03:27:20 +0530

    Add HTML and JavaScript files for alerts and prompts demonstration

---
+++ JavaScript/alerts.js
@@ -0,0 +1,8 @@
+console.log("This is a log message");
+console.error("This is an error message");
+console.warn("This is a warning message");
+
+let firstname=prompt("Enter your name: ");
+let lastname=prompt("Enter your last name: ");
+let msg= "Hello"+ " " + firstname + " " + lastname;
+alert(msg);
