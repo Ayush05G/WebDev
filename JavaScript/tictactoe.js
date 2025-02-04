9bb6c919d9515126f59c1f94dc8ee23fa9e5f641
Ayush Gupta <ayush2425.rk@gmail.com>
2025-02-04 23:39:01 +0530

    Add HTML and JavaScript files for TicTacToe game implementation

---
+++ JavaScript/tictactoe.js
@@ -0,0 +1,5 @@
+let game=[["X",null,"O"],[null,"X",null],["O",null,"X"]];
+//Now, let's say that the player wants to place an "O" in the top center cell.
+game[0][1] = "O";
+//The game array will now look like this:
+// [["X","O","O"],[null,"X",null],["O",null,"X"]]
