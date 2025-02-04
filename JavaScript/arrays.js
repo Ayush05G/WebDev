3e115e531b9a2d0f6dab7eb88e2a9df69e05afa2
Ayush Gupta <ayush2425.rk@gmail.com>
2025-02-04 23:30:40 +0530

    Add HTML and JavaScript files for array manipulation examples

---
+++ JavaScript/arrays.js
@@ -0,0 +1,19 @@
+let students = ['John', 'Jane', 'Jack', 'Jill', 'James'];
+students[0] = 'Jim';
+students.push('Jenny');
+students.pop();
+students.shift();
+students.unshift('Jill');
+let names2= ['John', 'Jane', 'Jack', 'Jill', 'James'];
+students.concat(names2);
+students.slice(1, 3);
+students.splice(1, 2, 'Jenny', 'Jill');
+students.sort();
+students.reverse();
+students.indexOf('Jenny');
+students.includes('Jenny');
+students.join(', ');
+const student = ['John', 'Jane', 'Jack', 'Jill', 'James'];
+let nums=[[1,2,3],[4,5,6],[7,8,9]];
+console.log(nums[1][1]);
+
