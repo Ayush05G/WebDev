66952cfded26b2ac78032545853ff3bae3c8adfe
Ayush Gupta <ayush2425.rk@gmail.com>
2025-02-09 06:00:05 +0530

    Add HTML and JavaScript files for demonstrating object usage

---
+++ JavaScript/objects.js
@@ -0,0 +1,23 @@
+let student = {
+    firstName: 'John',
+    lastName: 'Doe',
+    age: 25,
+};
+
+const classinfo = {
+    aman: {
+        name: 'Aman',
+        age: 25,
+        city: 'Delhi'
+    },
+    raman: {
+        name: 'Raman',
+        age: 26,
+        city: 'Mumbai'
+    },
+    shubham: {
+        name: 'Shubham',
+        age: 27,
+        city: 'Bangalore'
+    }
+};
